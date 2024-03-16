8f5f45bbd571b40843e5faf29f39dc668464e36b
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-16 12:21:28 -0300

    chore: bump version

--- src/index.ts
+++ src/index.ts
@@ -3,109 +3,231 @@ export interface ConfigOptions {
   include?: RegExp[];
   injectAtEnd?: boolean;
   imports?: any[];
-  manatsu?: boolean;
-  vueImports?: string[];
+  presets?: {
+    /** @default false */
+    manatsu?: boolean;
+    /** @default false */
+    manatsuStyle?: boolean;
+    /** @default true */
+    pinia?: boolean;
+    /** @default true */
+    router?: boolean;
+    /** @default true */
+    utilityTypes?: boolean;
+    /** @default true */
+    utils?: boolean;
+    /** @default true */
+    vue?: boolean;
+    /** @default true */
+    vueuse?: boolean;
+    /** @default false */
+    vueuseMath?: boolean;
+    /** @default false */
+    vueuseRouter?: boolean;
+  };
+}
+
+interface TypeImport {
+  from: string;
+  imports: string[];
+  type: true;
 }
 
 function config(options: ConfigOptions = {}) {
+  const {
+    manatsu = false,
+    manatsuStyle = false,
+    pinia = true,
+    router = true,
+    utilityTypes = true,
+    utils = true,
+    vue = true,
+    vueuse = true,
+    vueuseMath = false,
+    vueuseRouter = false
+  } = options.presets ?? {};
+
+  const imports: Record<string, string[]> = {};
+
+  if (manatsu) {
+    imports.manatsu = ['useElementSize'];
+  }
+
+  if (manatsuStyle) {
+    imports['@manatsu/style'] = ['css', 'tw'];
+  }
+
+  if (pinia) {
+    imports.pinia = ['getActivePinia', 'storeToRefs'];
+  }
+
+  if (router) {
+    imports['vue-router'] = ['useRoute', 'useRouter'];
+  }
+
+  if (utils) {
+    imports['@tb-dev/utils'] = ['isNullish'];
+  }
+
+  if (vue) {
+    imports.vue = [
+      'computed',
+      'customRef',
+      'defineComponent',
+      'effectScope',
+      'h',
+      'hasInjectionContext',
+      'inject',
+      'isRef',
+      'markRaw',
+      'nextTick',
+      'onActivated',
+      'onBeforeMount',
+      'onBeforeUnmount',
+      'onBeforeUpdate',
+      'onDeactivated',
+      'onMounted',
+      'onScopeDispose',
+      'onUnmounted',
+      'provide',
+      'readonly',
+      'ref',
+      'shallowReactive',
+      'shallowRef',
+      'reactive',
+      'toRaw',
+      'toRef',
+      'toValue',
+      'triggerRef',
+      'unref',
+      'watch',
+      'watchEffect',
+      'watchPostEffect',
+      'watchSyncEffect'
+    ];
+  }
+
+  if (vueuse) {
+    imports['@vueuse/core'] = [
+      'computedAsync',
+      'computedInject',
+      'computedWithControl',
+      'extendRef',
+      'onClickOutside',
+      'onKeyStroke',
+      'tryOnBeforeMount',
+      'tryOnBeforeUnmount',
+      'tryOnMounted',
+      'tryOnScopeDispose',
+      'tryOnUnmounted',
+      'until',
+      'useActiveElement',
+      'useArrayDifference',
+      'useArrayEvery',
+      'useArrayFilter',
+      'useArrayFind',
+      'useArrayFindIndex',
+      'useArrayFindLast',
+      'useArrayIncludes',
+      'useArrayJoin',
+      'useArrayMap',
+      'useArrayReduce',
+      'useArraySome',
+      'useArrayUnique',
+      'useAsyncState',
+      'useCurrentElement',
+      'useDraggable',
+      'useElementBounding',
+      'useElementHover',
+      'useElementVisibility',
+      'useEventListener',
+      'useFetch',
+      'useFocus',
+      'useFocusWithin',
+      'useIntersectionObserver',
+      'useLocalStorage',
+      'useMounted',
+      'useMouse',
+      'useMouseInElement',
+      'useMutationObserver',
+      'useObjectUrl',
+      'useOnline',
+      'usePageLeave',
+      'useParentElement',
+      'useResizeObserver',
+      'useSessionStorage',
+      'useStorage',
+      'useWindowScroll',
+      'useWindowSize',
+      'watchAtMost',
+      'watchDebounced',
+      'watchDeep',
+      'watchIgnorable',
+      'watchImmediate',
+      'watchOnce',
+      'watchPausable',
+      'watchThrottled',
+      'watchTriggerable',
+      'watchWithFilter',
+      'whenever'
+    ];
+
+    if (!manatsu) {
+      imports['@vueuse/core'].push('useElementSize');
+    }
+  }
+
+  if (vueuseRouter) {
+    imports['@vueuse/router'] = ['useRouteHash', 'useRouteParams', 'useRouteQuery'];
+  }
+
+  if (vueuseMath) {
+    imports['@vueuse/math'] = [
+      'logicAnd',
+      'logicNot',
+      'logicOr',
+      'useAbs',
+      'useAverage',
+      'useCeil',
+      'useFloor',
+      'useMax',
+      'useMin',
+      'useRound',
+      'useTrunc'
+    ];
+  }
+
+  const typeImports: TypeImport[] = [];
+
+  if (utilityTypes) {
+    typeImports.push({
+      from: '@tb-dev/utility-types',
+      imports: ['MaybeArray', 'MaybePromise', 'Nullish', 'PartialNullish'],
+      type: true
+    });
+  }
+
+  if (vue) {
+    typeImports.push({
+      from: 'vue',
+      imports: [
+        'CSSProperties',
+        'Component',
+        'ComponentInstance',
+        'MaybeRefOrGetter',
+        'Ref',
+        'ShallowRef',
+        'VNode'
+      ],
+      type: true
+    });
+  }
+
   return {
     dts: options.dts ?? './types/auto-imports.d.ts',
     injectAtEnd: options.injectAtEnd ?? true,
     include: [/\.[cm]?[jt]s$/, /\.vue$/, ...(options.include ?? [])],
-    imports: [
-      {
-        vue: [
-          'computed',
-          'customRef',
-          'defineComponent',
-          'effectScope',
-          'h',
-          'hasInjectionContext',
-          'inject',
-          'isRef',
-          'markRaw',
-          'nextTick',
-          'onActivated',
-          'onBeforeMount',
-          'onBeforeUnmount',
-          'onBeforeUpdate',
-          'onDeactivated',
-          'onMounted',
-          'onScopeDispose',
-          'onUnmounted',
-          'provide',
-          'readonly',
-          'ref',
-          'shallowReactive',
-          'shallowRef',
-          'reactive',
-          'toRaw',
-          'toRef',
-          'toValue',
-          'triggerRef',
-          'unref',
-          'watch',
-          'watchEffect',
-          'watchPostEffect',
-          'watchSyncEffect',
-          ...(options.vueImports ?? [])
-        ],
-
-        'vue-router': ['useRoute', 'useRouter'],
-        pinia: ['getActivePinia', 'storeToRefs'],
-
-        '@vueuse/core': [
-          'computedAsync',
-          'computedInject',
-          'computedWithControl',
-          'tryOnBeforeMount',
-          'tryOnBeforeUnmount',
-          'tryOnMounted',
-          'tryOnScopeDispose',
-          'tryOnUnmounted',
-          'useAsyncState',
-          'useIntersectionObserver',
-          'useLocalStorage',
-          'useMutationObserver',
-          'useResizeObserver',
-          'useSessionStorage',
-          'useStorage',
-          'watchAtMost',
-          'watchDeep',
-          'watchIgnorable',
-          'watchImmediate',
-          'watchOnce',
-          'watchPausable',
-          'watchTriggerable',
-          'whenever',
-
-          ...(options.manatsu ? [] : ['useElementSize'])
-        ],
-
-        manatsu: options.manatsu ? ['useElementSize'] : [],
-        '@manatsu/style': options.manatsu ? ['css', 'tw'] : []
-      },
-      {
-        from: 'vue',
-        imports: [
-          'CSSProperties',
-          'Component',
-          'ComponentInstance',
-          'MaybeRefOrGetter',
-          'Ref',
-          'ShallowRef',
-          'VNode'
-        ],
-        type: true
-      },
-      {
-        from: '@tb-dev/utility-types',
-        imports: ['MaybeArray', 'MaybePromise', 'Nullish', 'PartialNullish'],
-        type: true
-      },
-
-      ...(options.imports ?? [])
-    ]
+    imports: [imports, ...typeImports, ...(options.imports ?? [])]
   };
 }
 
