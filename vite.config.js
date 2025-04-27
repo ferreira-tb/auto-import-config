fd5b4bba39277e9de56a9063f514ee64b434bd25
Andrew Ferreira <andrew.shien2@gmail.com>
2025-04-27 12:07:01 -0300

    chore: update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
+import { join } from 'node:path';
 import dts from 'vite-plugin-dts';
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [dts({ rollupTypes: false })],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     minify: false,
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: join(import.meta.dirname, 'src/index.ts'),
       formats: ['es'],
       fileName: 'index'
     },
